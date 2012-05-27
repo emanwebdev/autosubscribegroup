2b1feb495070958f03ab1bc8aecbcb454ceb6ff4
Emmanuel <emanwebdev@gmail.com>
2012-05-27 16:37:43 +0200

    French translation pack

---
+++ autosubscribegroup/languages/fr.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Elgg autosubscribegroup plugin French language pack
+ * This plugin allows new users to get joined to groups automatically when they register.
+ *
+ * @package autosubscribegroups
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author RONNEL Jérémy
+ * @copyright (c) Elbee 2008
+ * @link /www.notredeco.com
+ *
+ * for Elgg 1.8 by iionly (iionly@gmx.de)
+ */
+
+$french = array(
+
+/**
+ * Menu items and titles
+ */
+'autosubscribe:list' => "IDs des Groupes, séparés par des virgules: ",
+);
+
+add_translation("fr",$french);
